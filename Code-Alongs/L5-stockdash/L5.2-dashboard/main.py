822713090dd9e873e009fc28f1b8cfc24edd0def
Lukas Källström AIU21 <89090277+LukasKallstrom@users.noreply.github.com>
2021-11-10 22:48:00 +0100

    finished lesson 5: stock dashboard

--- Code-Alongs/L5-stockdash/L5.2-dashboard/main.py
+++ Code-Alongs/L5-stockdash/L5.2-dashboard/main.py
@@ -5,6 +5,7 @@ import dash
 from load_data import StockDataLocal
 from dash.dependencies import Input, Output
 import plotly_express as px
+from time_filtering import filter_time
 
 stock_data_object = StockDataLocal()
 
@@ -17,6 +18,10 @@ stock_options_dropdown = [{"label": name, "value": symbol}
 df_dict = {symbol: stock_data_object.stock_dataframe(symbol)
            for symbol in symbol_dict}
 
+slider_marks = {i: mark for i, mark in enumerate(
+    ["1 Day", "1 Week", "1 Month", "3 Months", "1 Year", "5 Years", "Max"]
+)}
+
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -26,18 +31,33 @@ app.layout = html.Div([
                  options=stock_options_dropdown,
                  value='AAPL'
                  ),
-    dcc.Graph(id='stocks-graph', className='')
+    dcc.Graph(id='stocks-graph', className=''),
+
+    dcc.Slider(id='time-slider', className='',
+               min =0, max=6,
+               step=None,
+               value=3,
+               marks = slider_marks
+               )
 ])
 
 
 @app.callback(
     Output("stocks-graph", "figure"),
-    Input("stock-picker-dropdown", "value")
+    Input("stock-picker-dropdown", "value"),
+    Input("time-slider", "value")
 )
-def update_graph(stock):
+def update_graph(stock, time_index):
     dff_daily, dff_intraday = df_dict[stock]
 
-    fig = px.line(dff_daily, x=dff_daily.index, y="close")
+    dff = dff_intraday if time_index <= 2 else dff_daily
+
+    # maps 0-6 to number of days
+    days = {i: day for i,day in enumerate([1,7,30,90,365,365*5])}
+
+    dff = dff if time_index == 6 else filter_time(dff, days[time_index])
+
+    fig = px.line(dff, x=dff.index, y="close")
 
     return fig  # fig object goes into Output property i.e. figure property
 
